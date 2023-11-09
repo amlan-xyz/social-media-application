7de6c916e68d080b0af4dca32616b1d784f3b19a
theweird0ne <heyamlan19@gmail.com>
2023-11-09 16:02:15 +0530

    feat: post model created

---
+++ server/models/posts.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const postsSchema = new mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        comment: {
+          type: String,
+          required: true,
+        },
+        comment_by: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postsSchema);
+module.exports = Post;
