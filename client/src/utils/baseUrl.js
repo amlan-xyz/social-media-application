d48c89c24cd985faeb9121a7e36c8e6f2e071f30
theweird0ne <heyamlan19@gmail.com>
2023-12-07 14:12:34 +0530

    fix:change backend url

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://social-media-backend-m4q9.onrender.com";
+export const BASE_URL = "https://we-share-backend-4ore.onrender.com";
 // export const BASE_URL = "http://localhost:3001";
--- server/index.js
+++ server/index.js
@@ -9,25 +9,23 @@ app.use(express.json());
 
 //cors
 
-// let allowedDomains = [
-//   "http://localhost:3000/",
-//   "https://we-share-v1.vercel.app/",
-// ];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
+let allowedDomains = [
+  "http://localhost:3000/",
+  "https://we-share-v1.vercel.app/",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
 
-//       if (allowedDomains.indexOf(origin) === -1) {
-//         var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
-
-app.use(cors());
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 //routers
 const usersRouter = require("./routes/users.router");
